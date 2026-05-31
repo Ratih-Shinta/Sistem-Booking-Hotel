1f0dcc529c016ef158ab14969a637d12beedd3f1
kaoei <fahmiirfarizal@gmail.com>
2026-05-31 20:53:56 +0700

    node structure & insert room

---
+++ main.cpp
@@ -0,0 +1,206 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+Struktur node ku taro sini
+*/
+#define RED 0
+#define BLACK 1
+
+typedef struct tnodes {
+    int roomNumber;
+    char roomType[30];
+    int price;
+    char status[20];
+    char bookedBy[50];
+
+    int color;
+    struct tnodes *left, *right, *parent;
+} tnodes;
+
+tnodes *root = NULL;
+
+/*
+call function ditaro di sini
+*/
+tnodes* createNode(int roomNumber, char roomType[], int price);
+void leftRotate(tnodes *x);
+void rightRotate(tnodes *x);
+void fixInsert(tnodes *z);
+void insertRoom(int roomNumber, char roomType[], int price);
+void viewAllRooms(tnodes *node);
+void insertRoomMenu();
+
+
+/*
+ini function main
+*/
+int main() { 
+    insertRoomMenu();
+    return 0;
+}
+
+/*
+semua fitur function ditaro disini
+*/
+tnodes* createNode(int roomNumber, char roomType[], int price) {
+    tnodes* newNode = (tnodes*) malloc(sizeof(tnodes));
+
+    newNode->roomNumber = roomNumber;
+    strcpy(newNode->roomType, roomType);
+    newNode->price = price;
+    strcpy(newNode->status, "avail");
+    strcpy(newNode->bookedBy, "NULL");
+
+    newNode->color = RED;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->parent = NULL;
+
+    return newNode;
+}
+
+void leftRotate(tnodes *x) {
+    tnodes *y = x->right;
+    x->right = y->left;
+
+    if (y->left != NULL)
+        y->left->parent = x;
+
+    y->parent = x->parent;
+
+    if (x->parent == NULL)
+        root = y;
+    else if (x == x->parent->left)
+        x->parent->left = y;
+    else
+        x->parent->right = y;
+
+    y->left = x;
+    x->parent = y;
+}
+
+void rightRotate(tnodes *x) {
+    tnodes *y = x->left;
+    x->left = y->right;
+
+    if (y->right != NULL)
+        y->right->parent = x;
+
+    y->parent = x->parent;
+
+    if (x->parent == NULL)
+        root = y;
+    else if (x == x->parent->right)
+        x->parent->right = y;
+    else
+        x->parent->left = y;
+
+    y->right = x;
+    x->parent = y;
+}
+
+void fixInsert(tnodes *z) {
+    while (z->parent != NULL && z->parent->color == RED) {
+        tnodes *grandparent = z->parent->parent;
+
+        if (z->parent == grandparent->left) {
+            tnodes *uncle = grandparent->right;
+
+            if (uncle != NULL && uncle->color == RED) {
+                z->parent->color = BLACK;
+                uncle->color = BLACK;
+                grandparent->color = RED;
+                z = grandparent;
+            } else {
+                if (z == z->parent->right) {
+                    z = z->parent;
+                    leftRotate(z);
+                }
+
+                z->parent->color = BLACK;
+                grandparent->color = RED;
+                rightRotate(grandparent);
+            }
+        } else {
+            tnodes *uncle = grandparent->left;
+
+            if (uncle != NULL && uncle->color == RED) {
+                z->parent->color = BLACK;
+                uncle->color = BLACK;
+                grandparent->color = RED;
+                z = grandparent;
+            } else {
+                if (z == z->parent->left) {
+                    z = z->parent;
+                    rightRotate(z);
+                }
+
+                z->parent->color = BLACK;
+                grandparent->color = RED;
+                leftRotate(grandparent);
+            }
+        }
+    }
+
+    root->color = BLACK;
+}
+
+void insertRoom(int roomNumber, char roomType[], int price) {
+    tnodes *newNode = createNode(roomNumber, roomType, price);
+
+    tnodes *y = NULL;
+    tnodes *x = root;
+
+    while (x != NULL) {
+        y = x;
+
+        if (roomNumber < x->roomNumber)
+            x = x->left;
+        else if (roomNumber > x->roomNumber)
+            x = x->right;
+        else {
+            printf("Room number sudah ada!\n");
+            free(newNode);
+            return;
+        }
+    }
+
+    newNode->parent = y;
+
+    if (y == NULL)
+        root = newNode;
+    else if (roomNumber < y->roomNumber)
+        y->left = newNode;
+    else
+        y->right = newNode;
+
+    fixInsert(newNode);
+
+    printf("Room %d berhasil ditambahkan!\n", roomNumber);
+}
+
+void insertRoomMenu() {
+    int roomNumber;
+    char roomType[30];
+    int price;
+    char choice;
+
+    do {
+        printf("Nomor kamar  : ");
+        scanf("%d", &roomNumber);
+
+        printf("Tipe kamar   : ");
+        scanf(" %[^\n]", roomType);
+
+        printf("Harga        : ");
+        scanf("%d", &price);
+
+        insertRoom(roomNumber, roomType, price);
+
+        printf("\nInput kamar lagi? [y/n]: ");
+        scanf(" %c", &choice);
+
+    } while (choice == 'y' || choice == 'Y');
+}
